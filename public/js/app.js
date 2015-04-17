6551476a45b906f1408634f6904c2ee177ed88ae
feng <fw1714@mun.ca>
2015-04-17 16:15:56 -0700

    angular route adjust

--- public/js/app.js
+++ public/js/app.js
@@ -1,11 +1,14 @@
 angular.module("lolStat", ["ngRoute"])
     .config(function ($routeProvider) {
+        $routeProvider.when("/", {
+            templateUrl: "/views/user.html"
+        });
 
         $routeProvider.when("/rankings", {
             templateUrl: "/views/rankings.html"
         });
 
         $routeProvider.otherwise({
-            templateUrl: "/views/user.html"
+            redirectTo: '/'
         });
     });
