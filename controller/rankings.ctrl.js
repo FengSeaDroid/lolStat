378dc1b0f7865c0b4d29172e87728325e8c06f64
feng <fw1714@mun.ca>
2015-04-17 16:52:24 -0700

    error handling adjust

--- controller/rankings.ctrl.js
+++ controller/rankings.ctrl.js
@@ -6,14 +6,14 @@ module.exports = function (req, res) {
     var rankings = {};
     Rankings.findOne({queue: req.params.mode}, function (err, result) {
         if (err) {
-            return err;
+            res.status(400).json(err);
         } else {
             rankings = result;
             if (!rankings)
                 rankings = readAndSave(req.params.mode, res);
             else if (rankings.age > MAX_AGE) {
-                rankings.remove(function(err){
-                    if(err) return err;
+                rankings.remove(function (err) {
+                    if (err) res.status(400).json(err);
                     else rankings = readAndSave(req.params.mode, res);
                 });
             }
@@ -29,13 +29,17 @@ function readAndSave(mode, res) {
     require("superagent")
         .get('https://na.api.pvp.net/api/lol/na/v2.5/league/master?type=' + mode + '&api_key=48bb8ab1-2559-4225-949b-f9b45ea77e22')
         .end(function (err, data) {
-            var rankings = new Rankings(data.body);
-            rankings.save(function (saveerr) {
-                if (saveerr) {
-                    return saveerr;
-                } else {
-                    res.json(rankings);
-                }
-            });
+            if (err) {
+                res.status(400).json(err);
+            } else {
+                var rankings = new Rankings(data.body);
+                rankings.save(function (saveerr) {
+                    if (saveerr) {
+                        res.status(400).json(saveerr);
+                    } else {
+                        res.json(rankings);
+                    }
+                });
+            }
         });
 }
--- controller/user.detail.ctrl.js
+++ controller/user.detail.ctrl.js
@@ -4,7 +4,7 @@ exports.basic = function (request, response) {
     var user = {};
     UserBasic.findOne({name: request.params.name.toLowerCase()}, function (err, result) {
         if (err) {
-            return err;
+            response.status(400).json(err);
         } else {
             user = result;
             if (!user)
@@ -24,14 +24,18 @@ function readAndSaveBasic(username, response) {
     require("superagent")
         .get('https://na.api.pvp.net/api/lol/na/v1.4/summoner/by-name/' + username + '?api_key=48bb8ab1-2559-4225-949b-f9b45ea77e22')
         .end(function (err, data) {
-            var userBasic = new UserBasic(data.body[username]);
-            userBasic.name = userBasic.name.toLowerCase();
-            userBasic.save(function (saveerr) {
-                if (saveerr) {
-                    return saveerr;
-                } else {
-                    response.json(userBasic);
-                }
-            });
+            if (err) {
+                response.status(400).json(err);
+            } else {
+                var userBasic = new UserBasic(data.body[username]);
+                userBasic.name = userBasic.name.toLowerCase();
+                userBasic.save(function (saveerr) {
+                    if (saveerr) {
+                        response.status(400).json(saveerr);
+                    } else {
+                        response.json(userBasic);
+                    }
+                });
+            }
         });
 }
